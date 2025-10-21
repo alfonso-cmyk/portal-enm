e1a4e08d39017e26418a72be4a4de299affdbc70
alfonso-cmyk <alfonso@litenlemon.com>
2025-10-21 22:45:54 +0200

    Delete api directory

--- api/functions.js
+++
@@ -1,7 +0,0 @@
-import { base44 } from './base44Client';
-
-
-export const verificarFichajes = base44.functions.verificarFichajes;
-
-export const calcularImpuestosAutomaticos = base44.functions.calcularImpuestosAutomaticos;
-
